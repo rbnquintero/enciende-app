e8804e1e9c05dac60ebe2d4d844f974765615129
Ruben Quintero <ruben.quintero@jwmsolutions.com>
2016-05-10 13:10:29 -0500

    se arreglan mapas para android

---
+++ js/common/ActividadDetalleMapa.android.js
@@ -0,0 +1,64 @@
+import React, {
+  Component,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  TextInput,
+  View
+} from 'react-native';
+
+var MapView = require('react-native-maps');
+
+class ActividadDetalleMapa extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    if (this.props.actividad.horaInstrucciones == null) {
+      return null;
+    }
+
+    var lat = parseFloat(this.props.actividad.actividad.latitud);
+    var lon = parseFloat(this.props.actividad.actividad.longitudad);
+
+    return (
+      <MapView
+        style={styles.map}
+        showsUserLocation={true}
+        followsUserLocation={false}
+        initialRegion={{
+          latitude: lat,
+          longitude: lon,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}>
+        <MapView.Marker
+          coordinate={{
+            latitude: lat,
+            longitude: lon,
+            animateDrop: true,
+            draggable: false,
+          }}
+          title='{marker.title}'
+          description='{marker.description}'
+        />
+      </MapView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  titulo: {
+    flex: 1, fontWeight: '200', fontSize: 25, marginTop: 15,
+  },
+  texto: {
+    flex: 1, color: 'gray', fontWeight: '200', fontSize: 17, marginTop: 5,
+  },
+  map: {
+    flex: 1
+  },
+});
+
+module.exports = ActividadDetalleMapa;
